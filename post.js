7efccb46c0e86a33d6c663851a271df6fe6f8340
kaiyuan0708 <55030110+kaiyuan0708@users.noreply.github.com>
2020-03-25 02:18:38 +0800

    hapi post

---
+++ post.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'POST',
+        path: '/hello',
+        handler: function (request, h) {
+    
+            const payload = request.payload;
+    
+            return `Welcome ${payload.username}!`;
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
+
